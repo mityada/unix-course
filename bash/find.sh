389d05fcd0bd8a429c1ed04d12ce7078468f3ef4
Dmitry Deshevoy <mityada@gmail.com>
2012-10-03 01:24:15 +0400

    Support for all seven file types

--- bash/find.sh
+++ bash/find.sh
@@ -1,23 +1,17 @@
 #!/bin/bash
 
-getopts "-type:-iname:" $options
-echo $options
-
 shopt -s dotglob
 shopt -s nocaseglob
 
 path="."
 
-if [[ $# -gt 0 ]]; then
+if [[ $# -gt 0 && ${1} != -* ]]; then
     path=$1
     shift
 fi
 
-echo $path
-
 iname="*"
-print_files=1
-print_dirs=1
+filetype=""
 
 while [[ $# -gt 0 ]]
 do
@@ -29,38 +23,42 @@ do
         ;;
     -type)
         shift
-        case $1 in
-        f)
-            print_dirs=0
-            ;;
-        d)
-            print_files=0
-            ;;
-        esac
+        filetype=$1
+        if [[ ${filetype} != [bcdpfls] ]]; then
+            echo "find: Arguments to -type should contain only one letter"
+            exit
+        fi
+        shift
     esac
 done
 
 print_if_matches() {
-	if [[ ${1##*/} == $iname ]]; then
-		echo $1
-	fi
+    if [[ ${1##*/} == $iname ]]; then
+        if [[ $filetype == "" ]] ||
+           [[ ($filetype == "l" || !(-h $1)) &&
+           (($filetype == "b" && -b $1) ||
+            ($filetype == "c" && -c $1) ||
+            ($filetype == "d" && -d $1) ||
+            ($filetype == "p" && -p $1) ||
+            ($filetype == "f" && -f $1) ||
+            ($filetype == "l" && -h $1) ||
+            ($filetype == "s" && -S $1)) ]]; then
+            echo $1
+        fi
+    fi
 }
 
 print_dir() {
-	if [[ (-d $1 && $print_dirs == 1) || (!(-d $1) && $print_files == 1) ]]; then
-		print_if_matches $1	
-	fi
+    print_if_matches $1    
 
-	for file in $1/*
-	do
-		if [[ -d $file ]]; then
-			print_dir $file
-		else
-			if [[ $print_files == 1 ]]; then
-				print_if_matches $file
-			fi
-		fi
-	done
+    for file in $1/*
+    do
+        if [[ -d $file && !(-h $file) ]]; then
+            print_dir $file
+        else
+            print_if_matches $file
+        fi
+    done
 }
 
 print_dir $path
