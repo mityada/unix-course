9451dea041d5d1d7f47cd5105c9bbd56f7c85f1a
Dmitry Deshevoy <mityada@gmail.com>
2012-12-08 02:04:19 +0400

    [unsed] preprocessor directives

--- unsed/readlines.h
+++ unsed/readlines.h
@@ -1,4 +1,7 @@
-struct RL;
+#ifndef _READLINES_H
+#define _READLINES_H
+
+typedef struct RL RL;
 
 RL* rl_open(int fd, size_t max_size);
 
@@ -7,3 +10,5 @@ size_t rl_max_size(RL *rl);
 int rl_close(RL *rl);
 
 int rl_readline(RL *rl, char *buf, size_t buf_size);
+
+#endif
