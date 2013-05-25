b5a245d675d1ad9e86eaa232116e02808fc7f7d9
Dmitry Deshevoy <mityada@gmail.com>
2013-05-25 11:38:26 +0400

    [minissh] Fix formatting

--- minissh/minissh.c
+++ minissh/minissh.c
@@ -1,5 +1,5 @@
-#include<sys/types.h>
-#include<sys/socket.h>
+#include <sys/types.h>
+#include <sys/socket.h>
 #include <netdb.h>
 #include <stddef.h>
 #include <stdlib.h>
@@ -12,46 +12,52 @@ int main()
     struct addrinfo hints;
     struct addrinfo *servinfo;
     int status;
-    memset(&hints,0,sizeof hints);
-    hints.ai_family=AF_UNSPEC; //don’tcareIPv4orIPv6
-    hints.ai_socktype=SOCK_STREAM;//TCPstreamsockets
-    hints.ai_flags=AI_PASSIVE; //fillinmyIPforme
-    if ((status=getaddrinfo(NULL,"3490",&hints,&servinfo)) !=0){
-        fprintf(stderr,"getaddrinfo error:%s\n",gai_strerror(
-                    status));
+
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE;
+
+    if ((status = getaddrinfo(NULL, "3490", &hints, &servinfo)) !=0)
+    {
+        fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
         exit(1);
     }
+
     if (servinfo == NULL)
     {
         printf("servinfo == NULL\n");
         exit(1);
     }
-    int sockfd = socket(servinfo->ai_family, 
-            servinfo->ai_socktype, servinfo->ai_protocol);
+
+    int sockfd = socket(servinfo->ai_family, servinfo->ai_socktype, servinfo->ai_protocol);
     if (sockfd < 0)
     {
         perror("sockfd < 0");
         exit(1);
     }
-    if (bind(sockfd, servinfo->ai_addr, 
-                servinfo->ai_addrlen) < 0)
+
+    if (bind(sockfd, servinfo->ai_addr, servinfo->ai_addrlen) < 0)
     {
         perror("bind < 0");
         exit(1);
     }
+
     int yes = 1;
     if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) < 0)
     {
         perror("setsockopt < 0");
         exit(1);
     }
+
     if (listen(sockfd, 5) < 0)
     {
         perror("listen < 0");
         exit(1);
     }
+
     freeaddrinfo(servinfo);
-    int fd = -1;
+
     while (1)
     {
         int cfd = accept(sockfd, NULL, NULL);
@@ -69,11 +75,7 @@ int main()
             dup2(cfd, 1);
             dup2(cfd, 2);
             close(cfd);
-            //execvp("cat", "cat", "-", NULL);
-            //execlp("echo", "echo", "hello", NULL);
-            char buff[256];
-            read(0, buff, 256);
-            write(1, buff, 256);
+            execlp("bash", "bash", NULL);
             exit(0);
         }
         close(cfd);
